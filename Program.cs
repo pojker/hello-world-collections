b5c8579b00ff7dcc043029615780024470d27443
agata olczak-peuker <pojker@gmail.com>
2024-02-19 20:54:29 +0100

    initial commit - lists

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// CREATE LISTS
+
+
+var names = new List<string> { "aga", "Ana", "Felipe"};
+foreach (var name in names)
+{
+    Console.WriteLine($"Hello {name.ToUpper()}!");
+}
+
+
+/* Output:
+Hello AGA!
+Hello ANA!
+Hello FELIPE!
+*/
+
