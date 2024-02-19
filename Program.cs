5abb1d681a81f1450a3d156073146d540f95eebe
agata olczak-peuker <pojker@gmail.com>
2024-02-19 22:19:20 +0100

    Lists of other types

--- Program.cs
+++ Program.cs
@@ -181,3 +181,27 @@ Hello FELIPE!
 Hello MARIA!
 Hello AGA!
 */
+
+
+// LISTS OF OTHER TYPES
+
+var fibonacciNumbers = new List<int> {1, 1};
+
+var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+fibonacciNumbers.Add(previous + previous2);
+
+foreach(var item in fibonacciNumbers)
+{
+    Console.WriteLine(item);
+}
+
+
+/* Output
+1
+1
+2
+*/
+
+
